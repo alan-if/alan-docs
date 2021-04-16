3940208db6eab7f25e897453eae3b958ec917b52
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2021-04-16 11:39:17 +0200

    [design] Fix errors in publish script

--- alan-design/publish.sh
+++ alan-design/publish.sh
@@ -2,12 +2,15 @@
 #
 # Commit the generated Design Documentation files to the `published` branch
 # to get them published on https://alan-if.github.io/alan-docs/
+source=`git rev-parse --show-toplevel`
 echo Publishing design documentation...
 cd /tmp
 git clone -b published git@github.com:alan-if/alan-docs --single-branch
 cd alan-docs
 for f in design actors-in-containers rules
-         cp $source/alan-design/$f.html design/
+do
+    cp $source/alan-design/$f.html design/
+done
 git commit -am"[publish script] new version of design documentation"
 git push
 rm -rf alan-docs
