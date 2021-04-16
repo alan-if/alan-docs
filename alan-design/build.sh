a75428e0c01549b0e879011615b25b26cbd34a1d
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2021-04-16 14:14:25 +0200

    [design] Regenerate html:s with build.sh

--- alan-design/build.sh
+++ alan-design/build.sh
@@ -3,5 +3,5 @@
 # no-spurious dates - reproducible
 for f in design rules actors-in-containers
 do
-    asciidoctor -r asciidoctor-diagram -a data-uri -a icons=font -a reproducible -a sectanchors -a toc=left $f.asciidoc
+    asciidoctor -r asciidoctor-diagram -a data-uri -a icons=font -a linkattrs -a reproducible -a sectanchors -a toc=left $f.asciidoc
 done
