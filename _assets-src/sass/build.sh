d7f2ef52be905b8a7ec81b21ac57a2bf8fede657
tajmone <tajmone@libero.it>
2021-07-30 18:33:49 +0200

    Sass Tweaks
    Tweak the Sass build script in view of the upcoming Rouge integration.
    Now the generated docinfo file for Highlight is named `docinfo_HL.html`
    in the Sass folder, and deployed as `docinfo.html` in its dest folders.

--- _assets-src/sass/build.sh
+++ _assets-src/sass/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# "build.sh"                            v1.0.0 | 2021/05/24 | by Tristano Ajmone
+# "build.sh"                            v1.1.0 | 2021/07/30 | by Tristano Ajmone
 # ------------------------------------------------------------------------------
 # This script requires Dart Sass to be installed on the system:
 #      https://github.com/sass/dart-sass
@@ -9,29 +9,36 @@
 #      https://chocolatey.org/packages/sass
 # ------------------------------------------------------------------------------
 
-# Build highlight.js stylesheet
-###############################
-sass highlight-js.scss ../../_assets/hjs/styles/github.min.css
+rm -f *.css docinfo*.html
 
-# Build Highlight stylesheet
-############################
+## Build CSS Stylesheets
+########################
+sass highlight-js.scss ../../_assets/hjs/styles/github.min.css
 sass highlight.scss highlight.css
 
-# Create docinfo file from CSS:
+## Create docinfo Files from CSS
+################################
 
-echo "<style>"    >  docinfo.html
-cat highlight.css >> docinfo.html
-echo "</style>"   >> docinfo.html
+echo "<style>"    >  docinfo_HL.html
+cat highlight.css >> docinfo_HL.html
+echo "</style>"   >> docinfo_HL.html
 
 
 if [[ $(uname -s) == MINGW* ]];then
-	unix2dos docinfo.html
+	unix2dos docinfo*.html
 fi
 
-# Deploy docinfo file to dest folders (can't be used via relative paths):
+## Deploy docinfo Files to Dest Folders
+#######################################
+
+# docinfo files can't be used via relative paths!
+
+# =========
+# Highlight
+# =========
 
 DestDirs="alanguide conversion"
 for folder in ${DestDirs[*]} ; do
 	echo "../../$folder/"
-	cp docinfo.html "../../$folder/"
+	cp docinfo_HL.html "../../$folder/docinfo.html"
 done
