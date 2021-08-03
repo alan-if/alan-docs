b111f1ea291fd875b91fb746800c45073fbfaaed
tajmone <tajmone@libero.it>
2021-08-03 17:29:49 +0200

    Dev Styles Previews
    In `_dev/styles-tests/` folder, with the docs previewing samples for all
    backends:
    
    * New `html_build.sh` script that converts each document to HTML using
      a different highlighter so that for a source `<fname>.asciidoc`:
        * `<fname>_rouge.html`     — Rouge
        * `<fname>_highlight.html` — Highlight
        * `<fname>_hljs.html`      — highlight,js
    * Delete `HTML_BUILD.bat` and `BUILD_ALL.bat`.
    * Delete `README.md` since it has to re-written from scratch.
    * Fix ADoc sources:
        * Some deprecated syntaxes were causing warnings.
        * Tweak contents to support all three highlighters in HTML.

---
+++ _dev/styles-tests/html_build.sh
@@ -0,0 +1,91 @@
+#! /bin/bash
+
+echo -e "\n===================================================="
+echo      "Converting Test Docs to Standalone HTML Documents..."
+echo -e   "====================================================\n"
+
+rougeDir=$(cd ../../_assets/rouge/; pwd)
+highlightDir=$(cd ../../_assets/hl/; pwd)
+hljsDir=$(cd ../../_assets/hjs/; pwd)
+
+HtmlConvRouge() {
+	outname="${1%.*}_rouge.html"
+	asciidoctor \
+		--safe-mode unsafe \
+		--verbose \
+		--require $rougeDir/custom-rouge-adapter.rb \
+		-a source-highlighter=rouge \
+		-a rouge-style=thankful_eyes \
+		-a docinfodir=$rougeDir \
+		-a docinfo=shared-head \
+		-a data-uri \
+		-a icons=font \
+		-a linkattrs \
+		-a reproducible \
+		-a sectanchors \
+		-a toc=left \
+		-o $outname \
+		$1
+}
+
+HtmlConvHighlight() {
+	outname="${1%.*}_highlight.html"
+	asciidoctor \
+		--safe-mode unsafe \
+		--verbose \
+		--template-dir $highlightDir/haml \
+		--require $highlightDir/adoc/highlight-treeprocessor_mod.rb \
+		-a source-highlighter=highlight \
+		-a docinfodir=$highlightDir/adoc/ \
+		-a docinfo=shared-head \
+		-a data-uri \
+		-a icons=font \
+		-a linkattrs \
+		-a reproducible \
+		-a sectanchors \
+		-a toc=left \
+		-o $outname \
+		$1
+}
+
+HtmlConvHLJS() {
+	# GetOutFName "$1" "$2"
+	outname="${1%.*}_hljs.html"
+	asciidoctor \
+		--safe-mode unsafe \
+		--verbose \
+		-a source-highlighter=highlightjs \
+		-a highlightjsdir=$hljsDir \
+		-a data-uri \
+		-a icons=font \
+		-a linkattrs \
+		-a reproducible \
+		-a sectanchors \
+		-a toc=left \
+		-o $outname \
+		$1
+}
+rm -f *.html
+
+for f in *.asciidoc; do
+	echo -e "\e[0;34m=======================================\e[0m"
+	echo -e "\e[0;33m$f\e[0m"
+	echo -e "\e[0;34m=======================================\e[0m"
+	echo -e "\e[1;30m---------------------------------------\e[0;31m"
+	echo -e "\e[1;37m * * Highlight * *"
+	echo -e "\e[1;30m---------------------------------------\e[0;31m"
+	HtmlConvHighlight "$f"
+	echo -e "\e[1;30m---------------------------------------\e[0;31m"
+	echo -e "\e[1;37m * * highlight.js * *"
+	echo -e "\e[1;30m---------------------------------------\e[0;31m"
+	HtmlConvHLJS "$f"
+	echo -e "\e[1;30m---------------------------------------\e[0;31m"
+	echo -e "\e[1;37m * * Rouge * *"
+	echo -e "\e[1;30m---------------------------------------\e[0;31m"
+	HtmlConvRouge "$f"
+	echo -e "\e[0m"
+done
+
+# HtmlConvHLJS "styles-preview.asciidoc"
+# HtmlConvHLJS "tests-syntax-highlighting.asciidoc" "tests-syntax-highlighting_hljs"
+# HtmlConvHLJS "tests-typography.asciidoc"
