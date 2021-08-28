888a0c50c3ae1bc0fb7d0ef6fcd84efecd401212
Thomas Nilefalk <thomas@nilefalk.se>
2021-08-28 19:23:03 +0200

    [manual] Improve pre-amble and publish
    Closes #124

--- manual/publish.sh
+++ manual/publish.sh
@@ -2,18 +2,21 @@
 #
 # Commit the generated Manual files to the `published` branch
 # to get them published on https://alan-if.github.io/alan-docs/
+#
+# This script assumes you have recent .html and .pdf files
+#
 source=`git rev-parse --show-toplevel`
 case "`git branch --show-current`" in
-	"alan-manual-alpha")
-		stream=alpha
-		;;
-	"master")
-		stream=beta
-		;;
-	*)
-		echo "Unknown branch, can't publish"
-		exit
-		;;
+    "alan-manual-alpha")
+        stream=alpha
+        ;;
+    "master")
+        stream=beta
+        ;;
+    *)
+        echo "Unknown branch, can't publish"
+        exit
+        ;;
 esac
 echo Publishing manual for $stream stream...
 cd /tmp
