e8c44e85465166b63428a195799f0fea2a3cb2bb
Thomas Nilefalk <thomas@nilefalk.se>
2021-08-29 17:31:15 +0200

    [conversion guide] Add a publish script

---
+++ conversion/publish.sh
@@ -0,0 +1,16 @@
+#! /bin/bash -i
+#
+# Commit the generated Conversion guide in HTML format
+# to get them published on https://alan-if.github.io/alan-docs/
+#
+# This script assumes you have recent .html file
+#
+echo Publishing conversion guide...
+cd /tmp
+git clone -b published git@github.com:alan-if/alan-docs --single-branch
+cd alan-docs
+cp $source/conversion/conversion.html conversion/
+git commit -am"Publishing new version of conversion guide"
+git push
+cd ..
+#rm -rf alan-docs
