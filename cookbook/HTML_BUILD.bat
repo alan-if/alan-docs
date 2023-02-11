4ea74f9339cc1fdb431fc8ab32a11182a81e9c2d
Christian X. Winslett <winslett.christian@gmail.com>
2023-02-10 20:47:08 -0600

    Add Alan Cookbook

---
+++ cookbook/HTML_BUILD.bat
@@ -0,0 +1,29 @@
+:: "cookbook\HTML_BUILD.bat"              v1.1.2 | 2019/09/10 | by Tristano Ajmone
+:: -----------------------------------------------------------------------------
+:: This script is released into public domain via the Unlicense:
+::     http://unlicense.org/
+::
+:: To run this script these applications must be installed on your system:
+::
+::    Asciidoctor (Ruby Gem):
+::    https://github.com/asciidoctor/asciidoctor
+:: -----------------------------------------------------------------------------
+@ECHO.
+@ECHO ==============================================================================
+@ECHO Converting "The ALAN Language Cookbook" to a standalone HTML5 document...
+@ECHO ==============================================================================
+:: To run this script the following Ruby Gems must be installed on your system:
+::    https://github.com/asciidoctor/asciidoctor
+asciidoctor^
+  --safe-mode unsafe^
+  --verbose^
+  -a data-uri^
+  -a experimental^
+  -a icons=font^
+  -a reproducible^
+  -a sectanchors^
+  -a toc=left^
+  -a source-highlighter=highlightjs^
+  -a highlightjsdir=../_assets/hjs^
+  cookbook.asciidoc
+@EXIT /B
---
+++ cookbook/PDF_BUILD.bat
@@ -0,0 +1,44 @@
+:: "cookbook\PDF_BUILD.bat"               v3.0.1 | 2019/09/10 | by Tristano Ajmone
+:: -----------------------------------------------------------------------------
+:: This script is released into public domain via the Unlicense:
+::     http://unlicense.org/
+::
+:: To run this script these applications must be installed on your system:
+::
+::    Asciidoctor (Ruby Gem):
+::    https://github.com/asciidoctor/asciidoctor
+::
+::    asciidoctor-fopub:
+::    https://github.com/asciidoctor/asciidoctor-fopub
+:: -----------------------------------------------------------------------------
+@ECHO.
+@ECHO ==============================================================================
+@ECHO Converting "The ALAN Language Cookbook" to PDF document...
+@ECHO ==============================================================================
+:: To run this script the following tools must be installed on your system:
+::    https://github.com/asciidoctor/asciidoctor
+::    https://github.com/asciidoctor/asciidoctor-fopub
+
+:: Set some required env vars
+SET "CURRDIR=%CD%"
+SET "FOPUB_DIR=..\_assets\alan-xsl-fopub\"
+
+CALL asciidoctor^
+  -b docbook^
+  -d book^
+  -a data-uri!^
+  --safe-mode unsafe^
+  --verbose^
+  cookbook.asciidoc
+
+:: Need to switch working directory to "//_assets/alan-xsl-fopub/" for FOP:
+CD %FOPUB_DIR%
+
+CALL fopub^
+  -t xsl-fopub^
+  %CURRDIR%\cookbook.xml
+
+:: Restore origignal script working directory:
+CD %CURRDIR%
+
+@EXIT /B
